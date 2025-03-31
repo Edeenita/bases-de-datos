677edf5a3e8f6a232900730be93e2d457e876f1d
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2025-03-31 01:28:28 -0300

    tp4_ej11

--- tp4/tp4_ej11.sql
+++ tp4/tp4_ej11.sql
@@ -1,7 +1,9 @@
 set search_path = 'unc_esq_voluntario';
 
-SELECT  MIN(horas_aportadas) AS horas_minimas_aportadas,
+SELECT  nro_voluntario,
+        MIN(horas_aportadas) AS horas_minimas_aportadas,
         MAX(horas_aportadas) AS horas_maximas_aportadas,
         AVG(horas_aportadas) AS horas_promedio_aportadas
 FROM voluntario
-WHERE fecha_nacimiento >= '1990-1-1';
+WHERE fecha_nacimiento >= '1990-1-1'
+GROUP BY nro_voluntario
